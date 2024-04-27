7a4796d13da56f80759b8c0a0141d7e78cfa8161
Mosiur Rahman Sweet <sweet.csevu@gmail.com>
2024-04-27 20:28:01 +0600

    Added List of Fortunes

--- lib/screens/fortune_cookie_screen.dart
+++ lib/screens/fortune_cookie_screen.dart
@@ -9,6 +9,19 @@ class FortuneCookieScreen extends StatefulWidget {
 
 class _FortuneCookieScreenState extends State<FortuneCookieScreen> {
   @override
+  final List<String> _fortunes = [
+    "Opportunities are endless when you seize them with courage.",
+    "A journey of a thousand miles begins with a single step.",
+    "Your kindness will lead you to unexpected blessings.",
+    "Believe in yourself and others will too.",
+    "Wisdom is found not in knowing all the answers, but in asking the right questions.",
+    "Embrace change, for it is the only constant in life.",
+    "The best way to predict the future is to create it.",
+    "Happiness is not a destination, but a way of life.",
+    "Good things come to those who wait, but better things come to those who work for it.",
+    "Your smile is a key to unlock happiness in others."
+  ];
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
